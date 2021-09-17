d5d09c07c50f529f42d5c5e85e120b26193e6d3e
intkai <maaarineela@gmail.com>
2021-09-17 18:01:46 +0200

    first version

---
+++ project.java
@@ -0,0 +1,10 @@
+public class project {
+	
+	public static void main (String[] args) {
+		for(int i=1;i<=100;i++){
+			if(!(i%5!=0))
+				System.out.println(i);
+		}
+	}
+	//random ejercicio del año pasado he cambiado lo de dentro del if, habia un==
+}
