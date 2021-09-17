c8a9dcd1942ba6517c25cea4789566f20c93f771
jose3171 <joselito31072001@gmail.com>
2021-09-17 18:15:11 +0200

    Version 2.0

--- project.java
+++ project.java
@@ -2,9 +2,10 @@ public class project {
 	
 	public static void main (String[] args) {
 		for(int i=1;i<=100;i++){
-			if(!(i%5!=0))
+			if(!(i%5==0))
 				System.out.println(i);
 		}
 	}
 	//random ejercicio del año pasado he cambiado lo de dentro del if, habia un==
+
 }
